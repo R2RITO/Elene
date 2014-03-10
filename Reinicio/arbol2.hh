164973849ad94d9aaedf14db3a5e95c21adc5f18
Arturo <arturo.voltattorni@gmail.com>
2014-03-09 20:42:59 -0430

    Clases Legitimas

--- Reinicio/arbol2.hh
+++ Reinicio/arbol2.hh
@@ -89,5 +89,472 @@ public:
 };
 
 
+/* Clase para la disyuncion */
+class elene_DISYUNCION : public elene_EXPRBINARIA { 
+
+protected:
+
+    /* Metodo para imprimir */
+    virtual std::ostream& stream_write(std::ostream& os) const {              
+        return (os << "Disyuncion:\n" 
+                   << "  Expr Izq:\n" << (*expr_izq)
+                   << "  Expr Der:\n" << (*expr_der));
+    }
+
+public:
+
+    /* Constructor */
+    elene_DISYUNCION(elene_EXPR* izq, elene_EXPR* der): elene_EXPRBINARIA(izq, der) {}
+
+    /* Metodo para copiar */
+    elene_DISYUNCION(const elene_DISYUNCION &other) {
+        expr_izq = other.expr_izq;
+        expr_der = other.expr_der;
+    }
+
+    /* Metodo destructor */
+    virtual ~elene_DISYUNCION() {
+        delete expr_izq;
+        delete expr_der;
+    }
+
+    elene_DISYUNCION &operator = (const elene_DISYUNCION &other) {
+
+        if (&other != this) {
+
+            delete expr_izq;
+            delete expr_der;
+            expr_izq = other.expr_izq;
+            expr_der = other.expr_der;
+
+        }
+    }
+};
+
+/* Clase para la suma */
+class elene_ADICION : public elene_EXPRBINARIA { 
+
+protected:
+
+    /* Metodo para imprimir */
+    virtual std::ostream& stream_write(std::ostream& os) const {              
+        return (os << "Adicion:\n" 
+                   << "  Expr Izq:\n" << (*expr_izq)
+                   << "  Expr Der:\n" << (*expr_der));
+    }
+
+public:
+
+    /* Constructor */
+    elene_ADICION(elene_EXPR* izq, elene_EXPR* der): elene_EXPRBINARIA(izq, der) {}
+
+    /* Metodo para copiar */
+    elene_ADICION(const elene_ADICION &other) {
+        expr_izq = other.expr_izq;
+        expr_der = other.expr_der;
+    }
+
+    /* Metodo destructor */
+    virtual ~elene_ADICION() {
+        delete expr_izq;
+        delete expr_der;
+    }
+
+    elene_ADICION &operator = (const elene_ADICION &other) {
+
+        if (&other != this) {
+
+            delete expr_izq;
+            delete expr_der;
+            expr_izq = other.expr_izq;
+            expr_der = other.expr_der;
+
+        }
+    }
+};
+
+/* Clase para la resta */
+class elene_SUSTRACCION : public elene_EXPRBINARIA { 
+
+protected:
+
+    /* Metodo para imprimir */
+    virtual std::ostream& stream_write(std::ostream& os) const {              
+        return (os << "Sustraccion:\n" 
+                   << "  Expr Izq:\n" << (*expr_izq)
+                   << "  Expr Der:\n" << (*expr_der));
+    }
+
+public:
+
+    /* Constructor */
+    elene_SUSTRACCION(elene_EXPR* izq, elene_EXPR* der): elene_EXPRBINARIA(izq, der) {}
+
+    /* Metodo para copiar */
+    elene_SUSTRACCION(const elene_SUSTRACCION &other) {
+        expr_izq = other.expr_izq;
+        expr_der = other.expr_der;
+    }
+
+    /* Metodo destructor */
+    virtual ~elene_SUSTRACCION() {
+        delete expr_izq;
+        delete expr_der;
+    }
+
+    elene_SUSTRACCION &operator = (const elene_SUSTRACCION &other) {
+
+        if (&other != this) {
+
+            delete expr_izq;
+            delete expr_der;
+            expr_izq = other.expr_izq;
+            expr_der = other.expr_der;
+
+        }
+    }
+};
+
+
+/* Clase para la multiplicacion */
+class elene_MULTIPLICACION : public elene_EXPRBINARIA { 
+
+protected:
+
+    /* Metodo para imprimir */
+    virtual std::ostream& stream_write(std::ostream& os) const {              
+        return (os << "Multiplicacion:\n" 
+                   << "  Expr Izq:\n" << (*expr_izq)
+                   << "  Expr Der:\n" << (*expr_der));
+    }
+
+public:
+
+    /* Constructor */
+    elene_MULTIPLICACION(elene_EXPR* izq, elene_EXPR* der): elene_EXPRBINARIA(izq, der) {}
+
+    /* Metodo para copiar */
+    elene_MULTIPLICACION(const elene_MULTIPLICACION &other) {
+        expr_izq = other.expr_izq;
+        expr_der = other.expr_der;
+    }
+
+    /* Metodo destructor */
+    virtual ~elene_MULTIPLICACION() {
+        delete expr_izq;
+        delete expr_der;
+    }
+
+    elene_MULTIPLICACION &operator = (const elene_MULTIPLICACION &other) {
+
+        if (&other != this) {
+
+            delete expr_izq;
+            delete expr_der;
+            expr_izq = other.expr_izq;
+            expr_der = other.expr_der;
+
+        }
+    }
+};
+
+
+/* Clase para la division */
+class elene_DIVISION : public elene_EXPRBINARIA { 
+
+protected:
+
+    /* Metodo para imprimir */
+    virtual std::ostream& stream_write(std::ostream& os) const {              
+        return (os << "Division:\n" 
+                   << "  Expr Izq:\n" << (*expr_izq)
+                   << "  Expr Der:\n" << (*expr_der));
+    }
+
+public:
+
+    /* Constructor */
+    elene_DIVISION(elene_EXPR* izq, elene_EXPR* der): elene_EXPRBINARIA(izq, der) {}
+
+    /* Metodo para copiar */
+    elene_DIVISION(const elene_DIVISION &other) {
+        expr_izq = other.expr_izq;
+        expr_der = other.expr_der;
+    }
+
+    /* Metodo destructor */
+    virtual ~elene_DIVISION() {
+        delete expr_izq;
+        delete expr_der;
+    }
+
+    elene_DIVISION &operator = (const elene_DIVISION &other) {
+
+        if (&other != this) {
+
+            delete expr_izq;
+            delete expr_der;
+            expr_izq = other.expr_izq;
+            expr_der = other.expr_der;
+
+        }
+    }
+};
+
+/* Clase para el mayor que */
+class elene_MAYOR : public elene_EXPRBINARIA { 
+
+protected:
+
+    /* Metodo para imprimir */
+    virtual std::ostream& stream_write(std::ostream& os) const {              
+        return (os << "Mayor Que:\n" 
+                   << "  Expr Izq:\n" << (*expr_izq)
+                   << "  Expr Der:\n" << (*expr_der));
+    }
+
+public:
+
+    /* Constructor */
+    elene_MAYOR(elene_EXPR* izq, elene_EXPR* der): elene_EXPRBINARIA(izq, der) {}
+
+    /* Metodo para copiar */
+    elene_MAYOR(const elene_MAYOR &other) {
+        expr_izq = other.expr_izq;
+        expr_der = other.expr_der;
+    }
+
+    /* Metodo destructor */
+    virtual ~elene_MAYOR() {
+        delete expr_izq;
+        delete expr_der;
+    }
+
+    elene_MAYOR &operator = (const elene_MAYOR &other) {
+
+        if (&other != this) {
+
+            delete expr_izq;
+            delete expr_der;
+            expr_izq = other.expr_izq;
+            expr_der = other.expr_der;
+
+        }
+    }
+};
+
+
+/* Clase para el menor que */
+class elene_MENOR : public elene_EXPRBINARIA { 
+
+protected:
+
+    /* Metodo para imprimir */
+    virtual std::ostream& stream_write(std::ostream& os) const {              
+        return (os << "Menor Que:\n" 
+                   << "  Expr Izq:\n" << (*expr_izq)
+                   << "  Expr Der:\n" << (*expr_der));
+    }
+
+public:
+
+    /* Constructor */
+    elene_MENOR(elene_EXPR* izq, elene_EXPR* der): elene_EXPRBINARIA(izq, der) {}
+
+    /* Metodo para copiar */
+    elene_MENOR(const elene_MENOR &other) {
+        expr_izq = other.expr_izq;
+        expr_der = other.expr_der;
+    }
+
+    /* Metodo destructor */
+    virtual ~elene_MENOR() {
+        delete expr_izq;
+        delete expr_der;
+    }
+
+    elene_MENOR &operator = (const elene_MENOR &other) {
+
+        if (&other != this) {
+
+            delete expr_izq;
+            delete expr_der;
+            expr_izq = other.expr_izq;
+            expr_der = other.expr_der;
+
+        }
+    }
+};
+
+/* Clase para el menor o igual que */
+class elene_MENORIGUAL : public elene_EXPRBINARIA { 
+
+protected:
+
+    /* Metodo para imprimir */
+    virtual std::ostream& stream_write(std::ostream& os) const {              
+        return (os << "Menor O Igual Que:\n" 
+                   << "  Expr Izq:\n" << (*expr_izq)
+                   << "  Expr Der:\n" << (*expr_der));
+    }
+
+public:
+
+    /* Constructor */
+    elene_MENORIGUAL(elene_EXPR* izq, elene_EXPR* der): elene_EXPRBINARIA(izq, der) {}
+
+    /* Metodo para copiar */
+    elene_MENORIGUAL(const elene_MENOR &other) {
+        expr_izq = other.expr_izq;
+        expr_der = other.expr_der;
+    }
+
+    /* Metodo destructor */
+    virtual ~elene_MENORIGUAL() {
+        delete expr_izq;
+        delete expr_der;
+    }
+
+    elene_MENORIGUAL &operator = (const elene_MENORIGUAL &other) {
+
+        if (&other != this) {
+
+            delete expr_izq;
+            delete expr_der;
+            expr_izq = other.expr_izq;
+            expr_der = other.expr_der;
+
+        }
+    }
+};
+
+
+/* Clase para el mayor o igual que */
+class elene_MAYORIGUAL : public elene_EXPRBINARIA { 
+
+protected:
+
+    /* Metodo para imprimir */
+    virtual std::ostream& stream_write(std::ostream& os) const {              
+        return (os << "Mayor O Igual Que:\n" 
+                   << "  Expr Izq:\n" << (*expr_izq)
+                   << "  Expr Der:\n" << (*expr_der));
+    }
+
+public:
+
+    /* Constructor */
+    elene_MAYORIGUAL(elene_EXPR* izq, elene_EXPR* der): elene_EXPRBINARIA(izq, der) {}
+
+    /* Metodo para copiar */
+    elene_MAYORIGUAL(const elene_MAYORIGUAL &other) {
+        expr_izq = other.expr_izq;
+        expr_der = other.expr_der;
+    }
+
+    /* Metodo destructor */
+    virtual ~elene_MAYORIGUAL() {
+        delete expr_izq;
+        delete expr_der;
+    }
+
+    elene_MAYORIGUAL &operator = (const elene_MAYORIGUAL &other) {
+
+        if (&other != this) {
+
+            delete expr_izq;
+            delete expr_der;
+            expr_izq = other.expr_izq;
+            expr_der = other.expr_der;
+
+        }
+    }
+};
+
+
+/* Clase para el distinto a */
+class elene_DISTINTO : public elene_EXPRBINARIA { 
+
+protected:
+
+    /* Metodo para imprimir */
+    virtual std::ostream& stream_write(std::ostream& os) const {              
+        return (os << "Distinto:\n" 
+                   << "  Expr Izq:\n" << (*expr_izq)
+                   << "  Expr Der:\n" << (*expr_der));
+    }
+
+public:
+
+    /* Constructor */
+    elene_DISTINTO(elene_EXPR* izq, elene_EXPR* der): elene_EXPRBINARIA(izq, der) {}
+
+    /* Metodo para copiar */
+    elene_DISTINTO(const elene_DISTINTO &other) {
+        expr_izq = other.expr_izq;
+        expr_der = other.expr_der;
+    }
+
+    /* Metodo destructor */
+    virtual ~elene_DISTINTO() {
+        delete expr_izq;
+        delete expr_der;
+    }
+
+    elene_DISTINTO &operator = (const elene_DISTINTO &other) {
+
+        if (&other != this) {
+
+            delete expr_izq;
+            delete expr_der;
+            expr_izq = other.expr_izq;
+            expr_der = other.expr_der;
+
+        }
+    }
+};
+
+/* Clase para el igual a */
+class elene_IGUAL : public elene_EXPRBINARIA { 
+
+protected:
+
+    /* Metodo para imprimir */
+    virtual std::ostream& stream_write(std::ostream& os) const {              
+        return (os << "Distinto:\n" 
+                   << "  Expr Izq:\n" << (*expr_izq)
+                   << "  Expr Der:\n" << (*expr_der));
+    }
+
+public:
+
+    /* Constructor */
+    elene_IGUAL(elene_EXPR* izq, elene_EXPR* der): elene_EXPRBINARIA(izq, der) {}
+
+    /* Metodo para copiar */
+    elene_IGUAL(const elene_IGUAL &other) {
+        expr_izq = other.expr_izq;
+        expr_der = other.expr_der;
+    }
+
+    /* Metodo destructor */
+    virtual ~elene_IGUAL() {
+        delete expr_izq;
+        delete expr_der;
+    }
+
+    elene_IGUAL &operator = (const elene_IGUAL &other) {
+
+        if (&other != this) {
+
+            delete expr_izq;
+            delete expr_der;
+            expr_izq = other.expr_izq;
+            expr_der = other.expr_der;
+
+        }
+    }
+};
+
 
 # endif
